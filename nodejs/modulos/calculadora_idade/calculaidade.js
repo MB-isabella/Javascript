2b5546a5dd5258b1422a16eb8dfac61337f27df1
Isabella Maciel Bellettini <isamacielbellettini@yahoo.com>
2025-09-18 11:02:51 -0300

    versao ESModules corrigida

--- nodejs/modulos/calculadora_idade/calculaidade.js
+++ nodejs/modulos/calculadora_idade/calculaidade.js
@@ -1,6 +1,5 @@
 //códico que cria função para calcular idade a partir do ano atual e ano de nascimento
-function calcularIdade(nome, anoAtual, anoNascimento) {
+export function calcularIdade(nome, anoAtual, anoNascimento) {
     idade = anoAtual - anoNascimento;
     return `Olá, ${nome}! Em ${anoAtual} Você tem ${idade} anos.`;
 }
-module.exports = { calcularIdade };
