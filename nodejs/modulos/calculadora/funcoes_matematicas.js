63790096ac2a46e25e0c14babf7507c25abf0b61
Isabella Maciel Bellettini <isamacielbellettini@yahoo.com>
2025-09-18 11:00:01 -0300

    versao com a calculadora concertada

--- nodejs/modulos/calculadora/funcoes_matematicas.js
+++ nodejs/modulos/calculadora/funcoes_matematicas.js
@@ -1,26 +1,19 @@
 // crie e exporte as quatro operações matemáticas básicas: soma, subtração, multiplicação e divisão.
-function soma(a, b) {
+export function soma(a, b) {
     return a + b;
 }
 
-function subtracao(a, b) {
+export function subtracao(a, b) {
     return a - b;
 }
 
-function multiplicacao(a, b) {
+export function multiplicacao(a, b) {
     return a * b;
 }
 
-function divisao(a, b) {
+export function divisao(a, b) {
     if (b === 0) {
         return 'Erro: Divisão por zero não é permitida.';
     }
     return a / b;
 }
-
-module.exports = {
-    soma,
-    subtracao,
-    multiplicacao,
-    divisao
-};
